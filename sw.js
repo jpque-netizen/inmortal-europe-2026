cdb198ee4a2563a3c5254c9df0ae6f2404ac0e99
jpque-netizen <jpque@hotmail.com>
2026-07-12 08:21:23 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,28 @@
-const CACHE = 'europa-inmortal-v34';
-const ASSETS = ['./index.html', './manifest.json',
+const CACHE='immortal-europe-en-v11';
+const ASSETS=['./index.html','./manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c =>
-    Promise.all(ASSETS.map(a => c.add(a).catch(()=>{})))
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>
+    Promise.all(ASSETS.map(a=>c.add(a).catch(()=>{})))
   ));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  const url = e.request.url;
-  // PDF.js from CDN: cache-first, store on first fetch
-  if (url.includes('cdnjs.cloudflare.com')) {
+self.addEventListener('fetch',e=>{
+  const url=e.request.url;
+  if(url.includes('cdnjs.cloudflare.com')){
     e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
+      caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+        const copy=resp.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
         return resp;
       }))
     );
     return;
   }
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
-      caches.match('./index.html')
-    ))
-  );
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));
 });
